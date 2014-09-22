babdfb9422b9968b30d59908665d74649a1e1191
thaoha <havanthao93@gmail.com>
2014-09-22 11:39:05 +0700

    update css

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var framework = require('total.js');
 var http = require('http');
 
 var port = parseInt(process.env.PORT || '3000');
-var debug = true;
+var debug = false;
 
 framework.run(http, debug, port, '0.0.0.0');
